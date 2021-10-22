6110f212f0b7ad9fbce689c45a5332a507ab64a1
Evgeny <info@sitespring.ru>
2021-10-22 17:43:15 +0300

    create index file

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @author Evgeny Shevtsov, info@sitespring.ru
+ * @homepage https://sitespring.ru
+ * @licence Proprietary
+ */
+export {default as BaseClass} from "src/models/BaseClass"
+export {default as BaseModel} from "src/models/BaseModel"
+export {default as BaseProxy} from "src/models/BaseProxy"
+export {default as BaseStore} from "src/models/BaseStore"
